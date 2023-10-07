20191277bccb1aa4745b4ae672a94f1c4a78560b
van1ove <berezko2004@gmail.com>
2023-10-07 20:12:44 +0300

    created animator controller, idle and fly animations

---
+++ Assets/Resources/Scripts/AI/Robot/Drone/DroneAnimator.cs
@@ -0,0 +1,60 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace AI
+{
+    public class DroneAnimator : MonoBehaviour
+    {
+        public Animator Animator => _animator;
+
+        [SerializeField, Required] private DroneBehavior _robotBrain;
+        [SerializeField, Required] private Health _health;
+        [SerializeField, Required] private NavMeshAgent _navMeshAgent;
+        [SerializeField, Required] private Animator _animator;
+
+        [Header("Animator Parameters")]
+        [SerializeField, AnimatorParam(nameof(_animator), AnimatorControllerParameterType.Float)] private string _animatorRobotSpeed;
+        //[SerializeField, AnimatorParam(nameof(_animator), AnimatorControllerParameterType.Trigger)] private string _animatorStartJump;
+        //[SerializeField, AnimatorParam(nameof(_animator), AnimatorControllerParameterType.Trigger)] private string _animatorEndJump;
+        //[SerializeField, AnimatorParam(nameof(_animator), AnimatorControllerParameterType.Float)] private string _animatorAttackIndex;
+        [SerializeField, AnimatorParam(nameof(_animator), AnimatorControllerParameterType.Trigger)] private string _animatorAttack;
+        [SerializeField, AnimatorParam(nameof(_animator), AnimatorControllerParameterType.Trigger)] private string _animatorTakeDamage;
+        [SerializeField, AnimatorParam(nameof(_animator), AnimatorControllerParameterType.Trigger)] private string _animatorDeath;
+        //s[SerializeField, AnimatorParam(nameof(_animator), AnimatorControllerParameterType.Bool)] private string _animatorStan;
+
+        private void Start()
+        {
+            _health.OnTakeDamage.AddListener(() => _animator.SetTrigger(_animatorTakeDamage));
+            _health.OnDeath.AddListener(() => _animator.SetTrigger(_animatorDeath));
+
+            _robotBrain.OnAttack.AddListener(OnAttack);
+        }
+
+        private void OnAttack(int index)
+        {
+            //_animator.SetFloat(_animatorAttackIndex, index);
+            _animator.SetTrigger(_animatorAttack);
+        }
+
+        // private void OnEnable()
+        // {
+        //     _agentLinkMover.OnLinkStart += () => _animator.SetTrigger(_animatorStartJump);
+        //     _agentLinkMover.OnLinkEnd += () => _animator.SetTrigger(_animatorEndJump);
+        // }
+
+        private void Update()
+        {
+            _animator.SetFloat(_animatorRobotSpeed, _navMeshAgent.velocity.magnitude);
+            Debug.Log(_navMeshAgent.velocity.magnitude);
+            //_animator.SetBool(_animatorStan, _robotBrain.IsInStan);
+        }
+
+        // private void OnDisable()
+        // {
+        //     _agentLinkMover.OnLinkStart -= () => _animator.SetTrigger(_animatorStartJump);
+        //     _agentLinkMover.OnLinkEnd -= () => _animator.SetTrigger(_animatorEndJump);
+        // }
+        
+    }
+}
